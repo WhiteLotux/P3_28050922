00f907d30b37f81e579554f31ba1b8c42e959044
WhiteLotux <archpaladinsoul@gmail.com>
2024-02-08 00:56:30 -0400

    face4

--- controllers.js
+++ controllers.js
@@ -1,82 +1,70 @@
-//Librerias y dependencias
+const cookieParser = require('cookie-parser');
+const path = require('path');
 require('dotenv').config();
+var handlebars = require('express-handlebars')
+const Recaptcha = require('express-recaptcha').RecaptchaV2;
+const recaptcha = new Recaptcha('6LfOBUYpAAAAAN8w1Meatl-oxfr8w4EBhh2Cvsj1', '6LfOBUYpAAAAAO5ERFJseNoPrvc9X0ckgVGdPOkD');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
-const app = express();
-const path = require('path');
-const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-const baseDatosModels = require('./models/baseDeDatos.js');
-const {PASSWORD,ADMIN} = process.env;
-let login= false;
-let ext;
-//++++++++++++++++++++++++++++++++++++++
-function getContentType(extname) {
-  switch (extname) {
-    case '.html':
-      return 'text/html';
-    case '.css':
-      return 'text/css';
-    case '.js':
-      return 'text/javascript';
-    case '.json':
-      return 'application/json';
-    case '.png':
-      return 'image/png';
-    case '.jpg':
-      return 'image/jpg';
-    case 'jpeg':
-      return 'image/jpeg';
-    default:
-      return 'text/plain';
-  }
-}
-function getContentTypee(extname) {
-  switch (extname) {
-    case '.html':
-      return '.html';
-    case '.css':
-      return '.css';
-    case '.js':
-      return '.js';
-    case '.json':
-      return '.json';
-    case '.png':
-      return '.png';
-    case '.jpg':
-      return '.jpg';
-    case 'jpeg':
-      return '.jpeg';
-    default:
-      return null;
-  }
-}
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './static/uploads')
-  },
-  filename: function (req, file, cb) {
-    ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + Date.now() + getContentTypee(ext))
-  }
-})
 
-let upload = multer({ storage: storage });
+const app = express();
+
+const server = http.createServer(app);
+
+const {Server} = require('socket.io');
+
+const io = new Server(server);
+
+io.on('connection',(socket)=>{
+
+console.log('Un usuario se a conectado');
+
+socket.emit('mensajeServer', '¡Hola, cliente!');
+
+socket.on('disconnect',()=>{
+
+console.log('un usuario se a desconectado');
+
+});
+
+});
 
 //recursos que se van a cargar en el server 
 app.use(express.static(__dirname+'/static'));
 
-//-----------------------------------------------------------------
 //Configuración del Servidor
 app.set('view engine','ejs');//definimos el motor de plantilla con archivos ejs
 app.set('views',path.join(__dirname,"./views"));//definimos la ruta del motor de plantilla
 app.use(express.urlencoded({extended:false}));//permite recuperar los valores publicados en un request
-port = app.listen(5000);
-console.log('Servidor corriendo exitosamente en el puerto 5000');
 
+app.use(cookieParser());
+app.use(express.json());
+const jwt = require('jsonwebtoken');
+const bodyParser= require('body-parser');
+//app.use(bodyParser.urlencoded({extended: true}));
 
+const baseDatosModels = require('./models/baseDeDatos.js');
+const utils = require('./utils/uploadImg.js');
+//middleware para verificar cliente
+const {verifyToken} = require('./utils/JWT.js');
+//middleware para verificar admin
+const {verifyToken2} = require('./utils/JWT2.js');
+//Variables de entorno
+const {PASSWORD,ADMIN,port,secretKey2} = process.env;
+let ext;
+//--------------------------------------------------------------
+let storage = multer.diskStorage({
+  destination: function (req, file, cb){
+    cb(null, './static/uploads')
+  },
+  filename: function (req, file, cb) {
+    ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + utils.getContentType(ext))
+  }
+})
+
+let upload = multer({ storage: storage });
 
 //-----------------------------------------------------------
 //enruptamiento
@@ -85,36 +73,38 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/login',(req,res)=>{
-res.render('iniciarSesion.ejs');
+res.render('login.ejs');
 });
 
 
 app.post('/login',(req,res)=>{
-
  const {admin,password} = req.body;
-
+ const dato= {
+  admin,
+  password
+ }
    if(admin === ADMIN && password === PASSWORD){
-    login=true;
+    const token = jwt.sign(dato,secretKey2,{expiresIn:60 * 60 * 24});
+   // Guardar token en cookies
+    res.cookie('token2', token, { httpOnly: true, secure: true });
     res.redirect('/productos');
    }else{
-    login=false;
-    res.redirect('/iniciarSesion');
+    res.json({ERROR:'Contraseña o usuario incorrecto vuelve a intentarlo'});
    }
-
 });
   
 
-app.get('/add',(req,res)=>{
+app.get('/add',verifyToken2,(req,res)=>{
 res.render('add.ejs');
 });
 
 //---------------------------------------------------------
-app.get('/addImagen',(req,res)=>{
-res.render('addImagen.ejs');
+app.get('/addImagen/:id',verifyToken2,(req,res)=>{
+baseDatosModels.getImagen(req,res);
 });
 
 
-app.post('/addImagen',upload.single('img'),(req,res)=>{
+app.post('/addImagen/:id',upload.single('img'),(req,res)=>{ 
 baseDatosModels.aggIMG(req,res);
 });
 
@@ -124,12 +114,12 @@ baseDatosModels.aggDato(req,res);
 });
 
 
-app.get('/productos',(req,res)=>{
+app.get('/productos',verifyToken2,(req,res)=>{
   baseDatosModels.mostrarProductos(req,res);
 });
 //-------------------------------------------------------
 // GET /editar/:id
-app.get('/update/:id',(req, res) => {
+app.get('/update/:id',verifyToken2,(req, res) => {
 baseDatosModels.mostrarUpdate(req,res);
 
 });
@@ -140,7 +130,7 @@ app.post('/update/:id', (req, res) => {
 });
 //-------------------------------------------------------
 // GET /eliminar/:id
-app.get('/delete/:id', (req, res) => {
+app.get('/delete/:id',verifyToken2,(req, res) => {
  baseDatosModels.mostrarDelete(req,res);
 });
 //-------------------------------------------------------
@@ -149,11 +139,11 @@ app.post('/delete/:id', (req, res) => {
  baseDatosModels.deletee(req,res);
 });
 //------------------------------------------------------
-app.get('/categorias', (req, res) => {
+app.get('/categorias',verifyToken2,(req, res) => {
  baseDatosModels.getCategorias(req,res);
 });
 //-------------------------------------------------------
-app.get('/addCategorias', (req, res) => {
+app.get('/addCategorias',verifyToken2,(req, res) => {
  res.render('addcategoria.ejs');
 });
 //-------------------------------------------------------
@@ -161,7 +151,7 @@ app.post('/addcategorias', (req, res) => {
  baseDatosModels.postCategorias(req,res);
 });
 //-------------------------------------------------------
-app.get('/updateCategoria/:id',(req,res)=>{
+app.get('/updateCategoria/:id',verifyToken2,(req,res)=>{
  baseDatosModels.mostrarUpdateC(req,res);
 });
 //-------------------------------------------------------
@@ -169,8 +159,169 @@ app.post('/updateCategoria/:id',(req,res)=>{
 baseDatosModels.updateCateg(req,res);
 });
 //-------------------------------------------------------
-//Metodo para manejar rutas no encontradas
-app.get('/*',(req,res)=>{
-res.render('notfound.ejs')
+app.get('/eliminarCategoria/:id',(req,res)=>{
+baseDatosModels.deleteCategoriaGET(req,res);
+})
+//-------------------------------------------------------
+app.get('/clientes',verifyToken,(req,res)=>{
+  console.log('mostrando pagina la cliente!');
+baseDatosModels.ClientesGET(req,res);
+})
+//-------------------------------------------------------
+app.post('/cliente',(req, res) => {
+ baseDatosModels.filtrar(req,res);
 });
 //-------------------------------------------------------
+app.get('/clientico',(req, res) => {
+ baseDatosModels.filtrar2(req,res);
+});
+//-------------------------------------------------------
+//-------------------------------------------------------
+app.get('/ruta', (req, res) => {
+  const {nombre,codigo,precio,descripcion,calidad,cantidad,url} = req.query;
+
+  let datos = {
+    nombre:nombre,
+    codigo:codigo,
+    precio:precio,
+    descripcion:descripcion,
+    calidad:calidad,
+    cantidad:cantidad,
+    url:url
+  }
+
+  console.log(datos,'Valor de Busqueda--por fin');
+  res.render('buscar.ejs',{result:datos});
+
+});
+//-------------------------------------------------------
+app.get('/detalles/:id',verifyToken,(req,res)=>{
+baseDatosModels.detalles(req,res);
+});
+//-------------------------------------------------------
+//-------------------------------------------------------
+app.get('/loginUsers',(req,res)=>{
+baseDatosModels.loginUsers(req,res);
+});
+//------------------------------------------------------
+app.post('/loginUsers',(req,res)=> {
+  baseDatosModels.postLoginCliente(req,res);
+})
+//------------------------------------------------------
+app.get('/registroUsers',(req,res)=>{
+  baseDatosModels.registroUsers(req,res);
+});
+//------------------------------------------------------
+app.post('/registroUsuariosPost',recaptcha.middleware.verify,(req,res)=>{
+
+
+   if(!req.recaptcha.error){
+    // El reCAPTCHA se ha verificado correctamente
+     baseDatosModels.registroUsuariosPost(req,res); 
+  } else{
+    // El reCAPTCHA no se ha verificado correctamente
+    res.send('Debes validar el Recaptcha');
+  } 
+})
+//------------------------------------------------------
+app.get('/mensageDeRegistro',(req,res)=>{
+const registro = req.cookies.registro;
+if(typeof registro !== 'undefined'){
+  res.json({mensaje:registro});
+}else{
+  res.json({mensaje:false});
+}
+})
+//------------------------------------------------------
+app.get('/eliminarMensajeRegistro',(req,res)=>{
+
+if(typeof req.cookies.registro !== 'undefined'){
+ res.clearCookie('registro'); 
+ res.json({mensaje:'Mensaje_Eliminadooo'});
+}else{
+  res.json({mensaje:false});
+}
+
+})
+//------------------------------------------------------
+app.get('/comprar/:id',verifyToken,(req,res)=>{
+  res.clearCookie('transaccion');
+  baseDatosModels.comprar(req,res);
+});
+//------------------------------------------------------
+app.post('/comprarPost',async (req,res)=>{
+baseDatosModels.comprarPOST(req,res);
+})
+//------------------------------------------------------
+app.get('/transaction',(req,res)=>{
+
+const transaction = req.cookies.transaccion;
+
+if(typeof transaction !== 'undefined'){
+ console.log('transaction desde controllers',transaction);
+ res.json({transaction}); 
+}else{
+res.json({message:false});
+}
+
+});
+//------------------------------------------------------
+app.get('/eliminarTransaction',(req,res)=>{
+ res.clearCookie('transaccion');
+ res.json({message:'transaccion eliminada'});
+})
+//------------------------------------------------------
+app.get('/usuarios',verifyToken2,(req,res)=>{
+baseDatosModels.mostrarUsers(req,res);
+})
+//------------------------------------------------------
+//------------------------------------------------------
+app.get('/compras',verifyToken2,(req,res)=>{
+baseDatosModels.MostrarCompras(req,res);
+})
+//------------------------------------------------------
+app.get('/addUser',verifyToken2,(req,res)=>{
+baseDatosModels.addUsers(req,res);
+})
+//------------------------------------------------------
+app.post('/addUser',(req,res)=>{
+  baseDatosModels.addUsersPost(req,res);
+})
+//------------------------------------------------------
+app.get('/updateUser/:id',verifyToken2,(req,res)=>{
+baseDatosModels.updateUser(req,res);
+})
+//------------------------------------------------------
+app.post('/updateUser/:id',(req,res)=>{
+baseDatosModels.updateUserPost(req,res);
+})
+//------------------------------------------------------
+app.get('/deleteUser/:id',(req,res)=>{
+baseDatosModels.deleteUser(req,res);
+});
+//------------------------------------------------------
+app.get('/deleteCompra/:id',(req,res)=>{
+baseDatosModels.deleteCompra(req,res);
+})
+//------------------------------------------------------
+//logout cliente
+app.get('/logout',(req, res) => {
+  //metodo para borrar la cookie
+  res.clearCookie('token');
+  res.redirect('/');
+});
+//------------------------------------------------------
+//logout administrador
+app.get('/logout2',(req, res) => {
+  res.clearCookie('token2');
+  res.redirect('/');
+});
+//-------------------------------------------------------
+//Metodo para manejar rutas no encontradas
+app.get('/*',(req,res)=>{
+res.render('notfound.ejs');
+});
+//-------------------------------------------------------
+server.listen(port,()=>{
+  console.log(`Servidor corriendo exitosamente en el puerto ${port}`);
+});
